9d9d4add5f77e7b6089195a357893abc1764a6fe
Steve Matney <steve.matney@extendhealth.com>
2021-01-29 15:23:16 -0700

    Build: setting up release for react-17

--- release.config.js
+++ release.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   repositoryUrl: 'git@github.com:WTW-IM/react-html-element.git',
+  branches: [
+    { name: 'master' },
+    { name: 'react-17', channel: 'react-17', prerelease: 'react-17' },
+  ],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
@@ -8,10 +12,11 @@ module.exports = {
     [
       '@semantic-release/git',
       {
-        message: 'Docs: ${nextRelease.version} [skip ci]\n\n${nextRelease.note}'
-      }
+        message:
+          'Docs: ${nextRelease.version} [skip ci]\n\n${nextRelease.note}',
+      },
     ],
-    '@semantic-release/npm'
+    '@semantic-release/npm',
   ],
-  preset: 'eslint'
+  preset: 'eslint',
 };
