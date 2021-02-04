871755fc4b78fc557e77bf3b7bd6048480b3a34d
Steve Matney <steve.matney@gmail.com>
2021-02-03 22:43:10 -0700

    Fix: ensuring we only load the HTML 5 Adapter when we need it

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter';
+if (window.customElements) {
+  // eslint-disable-next-line global-require
+  require('@webcomponents/webcomponentsjs/custom-elements-es5-adapter');
+}
+// eslint-disable-next-line import/first
 import ReactHTMLElement from './ReactHTMLElement';
 
 export default ReactHTMLElement;
