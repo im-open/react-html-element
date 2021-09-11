a452be6c601727ec0fd667de7e7dd59b4d4432e2
Steve Matney <steve.matney@gmail.com>
2021-09-11 09:08:19 -0600

    Update: updating all dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ['./tsconfig.json', './src/__tests__/tsconfig.json']
+    project: ['./tsconfig.json', './src/__tests__/tsconfig.json'],
   },
   plugins: ['@typescript-eslint'],
   extends: [
@@ -10,12 +10,19 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'prettier/@typescript-eslint'
+    'plugin:import/recommended',
+    'plugin:react/recommended',
+    'prettier',
   ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
     'no-underscore-dangle': 'off',
     'max-len': ['error', { code: 150 }],
     'react/require-default-props': 0,
-    'react/prop-types': 0
-  }
+    'react/prop-types': 0,
+  },
 };
