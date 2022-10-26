4e75de49701e6dd98a2c310e74738ecc937e9823
Alex Abenoja <abenojaalex@gmail.com>
2022-10-26 14:16:45 -0600

    New: attributeStore and useWebComponentAttribute
    * allow convenient tracking of attribute changes with attributeStore and
      respond to updates to observedAttributes with useWebComponentAttribute

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter';
 import ReactHTMLElement from './ReactHTMLElement';
 
 export default ReactHTMLElement;
+export { attributeStore, useWebComponentAttribute } from './attributeStore';
