4819511c16f42a99c965c51108de992d11bfe567
Steve Matney <steve.matney@extendhealth.com>
2022-12-01 17:35:52 -0700

    Build: releasing development builds with PRs

--- release.config.js
+++ release.config.js
@@ -1,15 +1,38 @@
+const { execSync } = require('child_process');
+const MAIN_BRANCH = 'main';
+
+const branchName = execSync('git rev-parse --abbrev-ref HEAD')
+  .toString()
+  .replace('\n', '');
+
+const isMainBranch = branchName === MAIN_BRANCH;
+const githubBranchConfig = isMainBranch
+  ? {}
+  : {
+      successComment: `
+This PR is part of this prerelease version for testing: \${nextRelease.version}
+
+You can test it by using:
+\`\`\`bash
+npm install scriptloader-component@\${nextRelease.version}
+\`\`\`
+        `,
+    };
+
 module.exports = {
   repositoryUrl: 'git@github.com:WTW-IM/react-html-element.git',
   branches: [
     { name: 'main' },
     { name: 'react-17', channel: 'react-17', prerelease: 'react-17' },
     { name: 'react-16', channel: 'react-16' },
+    { name: '*', channel: 'development', prerelease: true },
   ],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    '@semantic-release/github',
+    '@semantic-release/npm',
+    ['@semantic-release/github', { ...githubBranchConfig }],
     [
       '@semantic-release/git',
       {
@@ -23,7 +46,6 @@ module.exports = {
         ],
       },
     ],
-    '@semantic-release/npm',
   ],
   preset: 'eslint',
 };
